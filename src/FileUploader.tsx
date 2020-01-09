8a31ce9fe3be65144bee4bda410cfa7b428b0309
Mateusz Pokora <mateusz.pokora@gmail.com>
2020-01-09 21:55:44 +0100

    style upload component

--- src/FileUploader.tsx
+++ src/FileUploader.tsx
@@ -1,12 +1,30 @@
 import React, { useCallback } from 'react';
 import axios from 'axios';
-import { useDropzone } from 'react-dropzone'
+import styled from 'styled-components';
+import { useDropzone } from 'react-dropzone';
+
+const FileUplaoderContainer = styled.div`
+  margin-top: 20px;
+`;
+
+const FileUploadZone = styled.div`
+  width: 100%;
+  height: 100px;
+  border: 2px solid #ddd;
+  border-radius: 4px;
+  border-style: dashed;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+`;
 
 const uploadFile = (file: File) => {
   const formData = new FormData();
   formData.append('file', file);
   axios.post('http://localhost:8080/api/v1/start-indexing', formData).then((response) => {
     console.log(response);
+    localStorage.setItem("fileUploaded", "true");
   });
 }
 
@@ -17,14 +35,17 @@ const FileUplaoder = () => {
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
   return (
-    <div {...getRootProps()}>
-      <input {...getInputProps()} />
-      {
-        isDragActive ?
-          <p>Drop the files here ...</p> :
-          <p>Drag 'n' drop some files here, or click to select files</p>
-      }
-    </div>
+    <FileUplaoderContainer>
+      <h3>Start by uploading your image set:</h3>
+      <FileUploadZone {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the files here ...</p> :
+            <p>Drag 'n' drop some files here, or click to select files</p>
+        }
+      </FileUploadZone>
+    </FileUplaoderContainer>
   )
 }
 
