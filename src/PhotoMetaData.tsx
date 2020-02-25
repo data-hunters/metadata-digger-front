cb0264ba8386e8849f5c82af87a4df895134cbc6
Mateusz Pokora <mateusz.pokora@gmail.com>
2020-02-25 20:26:58 +0100

    move metadata view to new page

--- src/PhotoMetaData.tsx
+++ src/PhotoMetaData.tsx
@@ -10,28 +10,40 @@ const PhotoMetaData: FC<PhotoMetaDataProps> = (props) => {
     return <div>Select photo to preview</div>
   }
   const { meta_data } = props.photo;
+  const fileName = props.photo.file_path.match(/[^/]*$/g);
+
   return (
-    <table className="table">
-      <thead>
-        <tr>
-          <th>Data</th>
-          <th>Value</th>
-        </tr>
-      </thead>
-      <tbody>
-        {
-          Object.keys(meta_data).map(key => {
-            const value = meta_data[key];
-            return (
-              <tr>
-                <td>{key}</td>
-                <td>{value.toString().substring(0, 400)}</td>
-              </tr>
-            )
-          })
-        }
-      </tbody>
-    </table>
+    <>
+      <div className="col-sm-6">
+        <img src={`data:image/png;base64, ${props.photo.thumbnail}`} alt='thumbnail' />
+        <h3>{fileName}</h3>
+
+      </div>
+      <div className="col-sm-6">
+
+        <table className="table">
+          <thead>
+            <tr>
+              <th>Data</th>
+              <th>Value</th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              Object.keys(meta_data).map(key => {
+                const value = meta_data[key];
+                return (
+                  <tr key={key}>
+                    <td>{key}</td>
+                    <td>{value.toString().substring(0, 400)}</td>
+                  </tr>
+                )
+              })
+            }
+          </tbody>
+        </table>
+      </div>
+    </>
   )
 }
 
