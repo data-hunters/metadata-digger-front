1365e8961ae9793786e1381fe4992aefea07835d
Michał Gutowski <michal@cucumbersome.net>
2020-05-24 09:16:16 +0200

    Remove strange casting

--- src/GraphContainer.tsx
+++ src/GraphContainer.tsx
@@ -1,6 +1,6 @@
 import { GraphState, GraphType, GRAPH_TYPES } from "./types";
 import React, { FC } from 'react';
-import { VictoryPie, VictoryChart, VictoryBar, VictoryTheme, VictoryTooltip } from 'victory';
+import { VictoryPie, VictoryTheme, VictoryTooltip } from 'victory';
 
 interface GraphContainerProps {
     graphState: GraphState;
@@ -9,23 +9,14 @@ interface GraphContainerProps {
 
 
 const GraphContainer: FC<GraphContainerProps> = (props) => {
-    
-    let stringToGraphType = (str: String): GraphType => {
-        if(str === "tag_names") return "tag_names"
-        else if (str === "directory_names") return "directory_names"
-        else if(str === "labels") return "labels"
-        else if(str === "file_type") return "file_type"
-        else if(str === "md_exif_ifd0_model") return "md_exif_ifd0_model"
-        else throw new Error("String" + str + "is not a GraphType")
-    }
-
     const dataForChart = Object.keys(props.graphState.values).map(key => {
         const value = props.graphState.values[key];
         return { y: value, label: key };
     })
+
     return(
         <div>
-            <select value={props.graphState.graphType} onChange={ e => props.changeType(stringToGraphType(e.target.value))} >
+            <select value={props.graphState.graphType} onChange={ e => props.changeType(e.target.value as GraphType)} >
                 {GRAPH_TYPES.map( (gt) => {
                     return <option value={gt} key={gt}>{gt}</option>
                 } )}
